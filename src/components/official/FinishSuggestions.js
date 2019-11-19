fa1d1151e8d28a55eedd6040826805de4c13a178
vdegraaf <vdegraaf.vincent@gmail.com>
2019-11-19 14:52:21 +0100

    added single dart option for finish

--- src/components/official/FinishSuggestions.js
+++ src/components/official/FinishSuggestions.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext, useState, useEffect, useRef } from 'react';
 import OfficialContext from '../../context/official/officialContext';
 import GameContext from '../../context/game/gameContext';
 import possibleFinishes from './FinishSuggestionsData';
@@ -6,21 +6,52 @@ import possibleFinishes from './FinishSuggestionsData';
 function FinishSuggestions({totalScore}) {
   const officialContext = useContext(OfficialContext);
   const gameContext = useContext(GameContext);
-  // let { players } = officialContext;
-  // const { current } = gameContext;
   let dart1;
+  let dart2;
+  const {current } = gameContext
 
-  let possibleFinish = possibleFinishes[totalScore]
+  const [possibleFinish, setPossibleFinish] = useState(possibleFinishes[totalScore])
+  let inputEl = useRef(null)
+  let inputElTwo = useRef(null)
+
+  useEffect(() => {
+    setPossibleFinish(possibleFinishes[totalScore])
+  }, [totalScore])
+
+  useEffect(() => {
+    clear()
+  }, [current])
+
+  const onChange = (e) => {
+    
+    setPossibleFinish(possibleFinishes[totalScore - e.target.value])
+  }
+
+  const clear = () => {
+    
+    inputEl.current.value = ""
+    inputElTwo.current.value = ""
+  }
 
   return (
     <div className='finish'>
-      <p style={{ fontSize: '12px' }}>Possible finish:</p> {possibleFinish}
+      <button onClick={clear}>CLEAR</button>
+      <p style={{ fontSize: '12px' }}>Possible finish: {possibleFinish}</p> 
       <input
         type='number'
         placeholder='dart 1'
         name='dart1'
         value={dart1}
-        // onChange={getPossibleFinish}
+        onChange={onChange}
+        ref={inputEl}
+      />
+      <input
+        type='number'
+        placeholder='dart 2'
+        name='dart2'
+        value={dart2}
+        onChange={onChange}
+        ref={inputElTwo}
       />
     </div>
   );
