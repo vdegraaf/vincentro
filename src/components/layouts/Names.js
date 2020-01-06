fccfec3563e509c74020f0d3b1a4dbc369748d00
vdegraaf <vdegraaf.vincent@gmail.com>
2020-01-06 16:21:32 +0100

    added switchPlayer button when tapping on name

--- src/components/layouts/Names.js
+++ src/components/layouts/Names.js
@@ -4,13 +4,13 @@ import GameContext from '../../context/game/gameContext';
 const Names = () => {
   const gameContext = useContext(GameContext);
 
-  const { players, current, game } = gameContext;
+  const { players, current, game, switchPlayer } = gameContext;
 
   return (
     <div className='container--names'>
       {players.map(player => {
         return (
-          <div key={player.id} className='box box-m'>
+          <div key={player.id} className='box box-m' onClick={() => switchPlayer()}>
             {current.name === player.name && game === '501' ? (
               <i className='fas fa-star' />
             ) : (
