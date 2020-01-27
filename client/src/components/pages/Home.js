635d331be99c2ae94c98551a5b127a77b614827a
vdegraaf <vdegraaf.vincent@gmail.com>
2020-01-27 11:08:57 +0100

    startgame btn works

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { Redirect } from 'react-router-dom';
+import GameContext from '../../context/game/gameContext';
 import Register from '../game/Register';
 import Type from '../game/Type';
 import StartGame from '../game/StartGame';
@@ -6,16 +8,30 @@ import AddPlayer from '../game/AddPlayer';
 import Player from '../game/Player';
 
 const Home = () => {
+  const gameContext = useContext(GameContext);
+  const { game } = gameContext;
+  const [redirect, setRedirect] = useState(false);
   const [isActive, setIsActive] = useState(false);
 
+
+  const startGame = () => {
+    setRedirect(true);
+    setTimeout(() => {
+      setRedirect(false);
+    }, 2000);
+  };
+
   return (
-    <div>
-      <Type />
-      <Player />
-      {isActive === false && <AddPlayer isActive={isActive} setIsActive={setIsActive} />}
-      {isActive && <Register isActive={isActive} setIsActive={setIsActive} />}
-      {isActive === false && <StartGame title='Start Game' />}
-    </div>
+    <>
+      {redirect && <Redirect to={`/${game}`} />}
+      <div>
+        <Type />
+        <Player />
+        {isActive === false && <AddPlayer isActive={isActive} setIsActive={setIsActive} />}
+        {isActive && <Register isActive={isActive} setIsActive={setIsActive} />}
+        {isActive === false && <StartGame title='Start Game' onClick={startGame} />}
+      </div>
+    </>
   );
 };
 
