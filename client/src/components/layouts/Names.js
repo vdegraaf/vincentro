90449f101865fb59f37496ee19b90210ab6483b6
vdegraaf <vdegraaf.vincent@gmail.com>
2020-01-27 15:31:33 +0100

    finished functionality cricket

--- client/src/components/layouts/Names.js
+++ client/src/components/layouts/Names.js
@@ -3,14 +3,14 @@ import GameContext from '../../context/game/gameContext';
 
 const Names = () => {
   const gameContext = useContext(GameContext);
-  const { players, current, switchPlayer } = gameContext;
+  const { players, current, switchPlayer, game } = gameContext;
 
   return (
     <div className='container container-names'>
       {players.map(player => {
         return (
           <div key={player.id} onClick={() => switchPlayer()}>
-            <h2 className={player.id === current.id ? 'isPlaying' : ''} >{player.nickname}</h2>
+            <h2 className={player.id === current.id && game === '501' ? 'isPlaying' : ''} >{player.nickname}</h2>
           </div>
         );
       })}
