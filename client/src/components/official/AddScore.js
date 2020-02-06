dccd0a4c21b0afa7ae484696b43c01438759eec1
vdegraaf <vdegraaf.vincent@gmail.com>
2020-02-06 12:25:55 +0100

    added keyboard

--- client/src/components/official/AddScore.js
+++ client/src/components/official/AddScore.js
@@ -3,21 +3,22 @@ import GameContext from '../../context/game/gameContext';
 import OfficialContext from '../../context/official/officialContext';
 import AddCircle from '../icons/AddCircle';
 import Previous from '../icons/Previous';
+import Keyboard from './Keyboard';
 
 const AddScore = () => {
   const gameContext = useContext(GameContext);
   const officialContext = useContext(OfficialContext);
 
   const { id } = gameContext.current;
-  const {switchPlayer} = gameContext
+  const { switchPlayer } = gameContext;
   const { addScore, deleteScore, players } = officialContext;
 
   const [score, setScore] = useState('');
 
   const validScoreCheck = () => {
-    
+
     const { totalScore } = players.find(player => player.id === id);
-   
+
     if (!totalScore) {
       return;
     }
@@ -28,8 +29,11 @@ const AddScore = () => {
   };
 
   const onChange = e => {
-    
-    setScore(e.target.value);
+    console.log('event',e)
+
+    setScore(score + e)
+    // if keyboard is not active this fn should be called
+    // setScore(e.target.value);
   };
 
   const onSubmit = e => {
@@ -38,35 +42,40 @@ const AddScore = () => {
     let points = validScoreCheck() ? 0 : score;
     addScore(points, id);
     setScore('');
-    
+
   };
 
   const previous = () => {
-    
-    switchPlayer()
-    deleteScore(id)
-  }
+
+    switchPlayer();
+    deleteScore(id);
+  };
 
   return (
-    <div className='addScore-container'>
-      <form onSubmit={onSubmit} className='addScore-form' >
-        <Previous previous={previous}/>
-        <div className='addScore-item '>
-          <input
-            type='number'
-            name='score'
-            placeholder='Score'
-            value={score}
-            onChange={onChange}
-            required
-            min='0'
-            max='180'
-            className='addScore-input'
-          />
-        </div>          
-        <AddCircle onSubmit={onSubmit}/>  
-      </form>
-    </div>
+    <>
+      <div className='addScore-container'>
+        <form onSubmit={onSubmit} className='addScore-form' >
+          <Previous previous={previous} />
+          <div className='addScore-item '>
+            <input
+              type='number'
+              name='score'
+              placeholder='Score'
+              value={score}
+              onChange={onChange}
+              required
+              min='0'
+              max='180'
+              className='addScore-input'
+            />
+          </div>
+          <AddCircle onSubmit={onSubmit} />
+        </form>
+      </div>
+
+      <Keyboard onChange={onChange} score={score}/>
+
+    </>
   );
 };
 
---
+++ client/src/components/official/Keyboard.js
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react';
+import Backspace from '../icons/BackSpace'
+
+
+const Keyboard = ({onChange, score}) => {
+
+  const keys = ['1', '2', '3', '4', '5', '6', '7', '8', '9', ' ', '0']
+  
+  
+  
+  return (
+    <div className="keyboard-container">
+      {keys.map((key, i) => {
+        return <div 
+        onClick={(e) => onChange(e.currentTarget.dataset.value)} 
+        key={i} data-value={key}
+        className="keyboard-item"
+        >{key}</div>
+      })}
+      <div className="keyboard-item"><Backspace/></div>
+    </div>
+  )
+};
+
+export default Keyboard;
